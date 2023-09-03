ffe12aaea51556ff340ec4bd1e4e02437a1428aa
hashimoto <shuji8519@gmail.com>
2023-09-03 15:35:12 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 $(function () {
-  // slick.js
-  // $(function () {
-  //   $("#js-slider-2").slick({
-  //     arrows: false, // 前・次のボタンを表示する
-  //     dots: false, // ドットナビゲーションを表示する
-  //     appendDots: $(".dots-2"), // ドットナビゲーションの生成位置を変更
-  //     speed: 1000, // スライドさせるスピード（ミリ秒）
-  //     slidesToShow: 1, // 表示させるスライド数
-  //     centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
-  //     variableWidth: true, // スライド幅の自動計算を無効化
-  //     autoplay: true,
-  //     infinite: true, //永久にループさせる
-  //   });
-  // });
-});
-
-$(function () {
   $(".slider").slick({
     arrows: false, // 前・次のボタンを表示する
     dots: false, // ドットナビゲーションを表示する
@@ -26,7 +9,8 @@ $(function () {
     //     appendDots: $(".dots-2"), // ドットナビゲーションの生成位置を変更
   });
 });
-////////////////
+
+// アコーディオン
 
 $(function () {
   // タイトルをクリックすると
@@ -36,4 +20,37 @@ $(function () {
     // タイトルにopenクラスを付け外しして　−を表示
     $(this).toggleClass("open", 300);
   });
+});
+
+// モーダル
+$(function () {
+  "use strict";
+
+  //モーダル表示
+  if ($(".js-modal-target").length > 0) {
+    $(".js-modal-target").modaal();
+  }
+  if ($(".js-modal-button-target").length > 0) {
+    var target = $(".js-modal-button-target").data("modal");
+    $(".js-modal-button-target").modaal({
+      content_source: target,
+    });
+  }
+})(jQuery);
+
+$(".js-modal-close").on("click", function () {
+  // モーダルを閉じる
+  $(".modal").modaal("close");
+});
+
+$(".modal").modaal({
+  type: "inline",
+  hide_close: false, // 閉じるボタンの表示・非表示の制御する
+  background: "#000000", // 背景オーバーレイの色を設定
+  overlay_opacity: 0.8, // 背景オーバーレイの透明度を設定
+  overlay_close: true, // 背景オーバーレイをクリックしてモーダルが閉じるかどうかの制御
+  animation_speed: 300, // トリガーをクリックしてから、モーダルが表示されるまでのスピード
+  background_scroll: false, // モーダルを開いている間、背景をスクロールできるかどうか
+  width: 400, // モーダルの幅を設定
+  height: 300, // モーダルの高さを設定
 });
